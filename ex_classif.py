fa0730cd11b663e89d462e28dd666f6f3b7f9f8c
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2017-06-06 17:31:13 +0200

    Semi-sparse Group Lasso implemented

--- ex_classif.py
+++ ex_classif.py
@@ -6,15 +6,20 @@ __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 
 n = 1000
 d = 20
-groups = numpy.array([0] * int(d / 2) + [1] * (d - int(d / 2)))
+groups = []
+gr0 = numpy.zeros((d, ), dtype=numpy.int32)
+gr0[:d//2] = 1
+gr1 = 1 - gr0
+groups.append(gr0)
+groups.append(gr1)
 alpha = 0.
 lbda = .1
 
 X = numpy.random.randn(n, d)
 secret_beta = numpy.random.randn(d)
-ind_sparse = numpy.zeros((d, ))
+ind_sparse = numpy.zeros((d, ), dtype=numpy.int32)
 for i in range(d):
-    if groups[i] == 0 or i % 2 == 0:
+    if groups[0][i] == 1 or i % 2 == 0:
         secret_beta[i] = 0
     if i % 2 == 0:
         ind_sparse[i] = 1
@@ -23,12 +28,12 @@ y = numpy.ones((n, 2), dtype=numpy.int32)
 y[numpy.exp(numpy.dot(X, secret_beta)) < 1., 0] = 0
 y[:, 1] = 1 - y[:, 0]
 
-model = SSGL_LogisticRegression(dim_input=d, n_classes=2, groups=groups, sparse_indices=ind_sparse)
+model = SSGL_LogisticRegression(dim_input=d, n_classes=2, groups=groups, indices_sparse=ind_sparse, n_iter=1000,
+                                alpha=alpha, lbda=lbda)
 
 model.fit(X, y)
-# beta_hat = model.coef_
-#
-# print(numpy.linalg.norm(secret_beta - beta_hat))
-# for i, (betai_hat, betai) in enumerate(zip(beta_hat, secret_beta)):
-#     print("Component %02d: %.4f | %.4f" % (i, betai_hat, betai))
+beta_hat = model.weights_
+
+for i, (betai_hat, betai) in enumerate(zip(beta_hat, secret_beta)):
+    print("Component %02d: %r | %.4f" % (i, betai_hat, betai))
 print("Correct classification rate: %.3f" % (numpy.sum(model.predict(X) == numpy.argmax(y, axis=1)) / n))
--- ssgl_classifiers.py
+++ ssgl_classifiers.py
@@ -1,31 +1,40 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.regularizers import Regularizer
+from keras import backend as K
 import numpy
 
 __author__ = 'Romain Tavenard romain.tavenard[at]univ-rennes2.fr'
 
 
 class SSGL_LogisticRegression:
-    def __init__(self, dim_input, n_classes, groups, sparse_indices, activation="relu", n_iter=100, batch_size=32):
+    def __init__(self, dim_input, n_classes, groups, indices_sparse, alpha=0.5, lbda=0.01, activation="relu",
+                 n_iter=500, batch_size=32):
         self.d = dim_input
         self.n_classes = n_classes
         self.groups = groups
-        self.sparse_indices = sparse_indices
+        self.indices_sparse = indices_sparse
         self.activation = activation
         self.n_iter = n_iter
         self.batch_size = batch_size
+        self.alpha = alpha
+        self.lbda = lbda
 
         self.model = None
+        self.regularizer = None
         self._init_model()
 
+    @property
+    def weights_(self):
+        return self.model.get_weights()[0]
+
     def _init_model(self):
+        self.regularizer = SSGL_WeightRegularizer(l1_reg=self.alpha * self.lbda, indices_sparse=self.indices_sparse,
+                                                  l2_reg=(1. - self.alpha) * self.lbda, groups=self.groups)
         self.model = Sequential()
-        self.model.add(Dense(units=self.n_classes, input_dim=self.d, activation='softmax'))
-        if self.n_classes == 2:
-            loss_str = "binary_crossentropy"
-        else:
-            loss_str = "categorical_crossentropy"
-        self.model.compile(loss=loss_str, optimizer="sgd")
+        self.model.add(Dense(units=self.n_classes, input_dim=self.d, activation="softmax",
+                             kernel_regularizer=self.regularizer))
+        self.model.compile(loss="categorical_crossentropy", optimizer="sgd")
 
     def fit(self, X, y):
         self.model.fit(X, y, epochs=self.n_iter, batch_size=self.batch_size, verbose=0)
@@ -36,3 +45,26 @@ class SSGL_LogisticRegression:
     def predict(self, X):
         probas = self.predict_probas(X)
         return numpy.argmax(probas, axis=1)
+
+
+class SSGL_WeightRegularizer(Regularizer):
+    def __init__(self, l1_reg=0., l2_reg=0., groups=None, indices_sparse=None):
+        self.l1_reg = l1_reg
+        self.l2_reg = l2_reg
+        if groups is None:
+            self.groups = []
+        else:
+            self.groups = [K.variable(gr.reshape((1, -1))) for gr in groups]
+        if indices_sparse is not None:
+            self.indices_sparse = K.variable(indices_sparse.reshape((1, -1)))
+
+    def __call__(self, x):
+        loss = 0.
+        if self.indices_sparse is not None:
+            loss += K.sum(K.dot(self.indices_sparse, K.abs(x))) * self.l1_reg
+        for gr in self.groups:
+            loss += K.sum(K.dot(gr, K.square(x))) * self.l2_reg
+        return loss
+
+    def get_config(self):
+        return {"name": self.__class__.__name__, "l1_reg": self.l1_reg, "l2_reg": self.l2_reg}
